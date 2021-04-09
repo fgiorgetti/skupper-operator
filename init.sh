74a0412e3a235490e789438cbadbf92cd2d1ab29
Fernando Giorgetti <fgiorget@redhat.com>
2021-04-09 14:18:04 -0300

    Added arg to init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ kubectl create ns skupper-sandbox
 kubectl apply -f catalog-ocp.yaml
 waitRunning openshift-marketplace skupper-operator
 
+[[ $1 == "catalog" ]] && exit 0
+
 kubectl apply -f operatorgroup-ocp.yaml
 kubectl apply -f subscription-ocp.yaml
 waitRunning skupper-sandbox skupper-site-controller
